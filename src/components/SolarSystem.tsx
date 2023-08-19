7d13d58cec7d35369dc93966034bdad9cb4d8dc9
Barcelos <barcelosps41710@gmail.com>
2023-08-18 21:26:56 -0300

    requesito 7

--- src/components/SolarSystem.tsx
+++ src/components/SolarSystem.tsx
@@ -1,9 +1,21 @@
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 function SolarSystem() {
   return (
     <div data-testid="solar-system">
+
       <Title headline="Planetas" />
+
+      {planets.map(({ name, image }) => (
+        <PlanetCard
+          key={ name }
+          planetName={ name }
+          planetImage={ image }
+        />
+      ))}
+      ;
     </div>
   );
 }
