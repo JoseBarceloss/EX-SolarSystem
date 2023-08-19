b79502791dcb5d6224cc9572643f321ddb15d7e2
Barcelos <barcelosps41710@gmail.com>
2023-08-18 22:37:23 -0300

    foi <3 exercise completed

--- src/components/Missions.tsx
+++ src/components/Missions.tsx
@@ -1,9 +1,20 @@
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 function Missions() {
   return (
     <div data-testid="missions">
       <Title headline="Missões" />
+      {missions.map((mission) => (
+        <MissionCard
+          key={ mission.name }
+          name={ mission.name }
+          year={ mission.year }
+          country={ mission.country }
+          destination={ mission.destination }
+        />
+      ))}
     </div>
   );
 }
--- src/components/SolarSystem.tsx
+++ src/components/SolarSystem.tsx
@@ -15,7 +15,7 @@ function SolarSystem() {
           planetImage={ image }
         />
       ))}
-      ;
+
     </div>
   );
 }
